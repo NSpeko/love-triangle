5cc80c9eb66c2cf3241cfeed53c913bf667016fe
NSpeko <nonstoper42@gmail.com>
2018-02-23 11:19:14 +0300

    Feature: Resolved love-triangles problem

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var triangleCount=0;
+    for(i=0;i<preferences.length;i++){
+        if(preferences[preferences[preferences[i]-1]-1]-1===i) triangleCount++;
+    }
+    return Number.parseInt(triangleCount/3);
 };
